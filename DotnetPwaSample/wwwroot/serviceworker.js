109a3aafc611d69e28cc4543d777a48919fc5295
Konrad Müller <k.l.mueller@outlook.com>
2018-08-14 00:05:52 +0200

    Reworked cached files

--- DotnetPwaSample/wwwroot/serviceworker.js
+++ DotnetPwaSample/wwwroot/serviceworker.js
@@ -1,4 +1,4 @@
-﻿console.log("This is service worker talking");
+﻿console.log("This is service worker talking!");
 var cacheName = 'blazor-pwa-sample';
 var filesToCache = [
     './',
@@ -6,12 +6,10 @@ var filesToCache = [
     './index.html',
     './css/site.css',
     './css/bootstrap/bootstrap.min.css',
-    './css/open-iconic/font/css/open-iconic-bootstrap.min.css',
-    './open-iconic',
+    './css/open-iconic/font/css/open-iconic-bootstrap.min.css', 
     './css/open-iconic/font/fonts/open-iconic.woff',
-    './css/loading.css',
     //Blazor framework
-    '_framework/blazor.webassembly.js',
+    './_framework/blazor.webassembly.js',
     './_framework/blazor.boot.json',
     //Our additional files
     './manifest.json',
@@ -20,20 +18,18 @@ var filesToCache = [
     './icons/icon-512x512.png',
     //The web assembly/.net dll's
     './_framework/wasm/mono.js',
-    './_framework/wasm/mono.wasm', 
+    './_framework/wasm/mono.wasm',
     './_framework/_bin/Microsoft.AspNetCore.Blazor.Browser.dll',
     './_framework/_bin/Microsoft.AspNetCore.Blazor.dll',
     './_framework/_bin/Microsoft.Extensions.DependencyInjection.Abstractions.dll',
     './_framework/_bin/Microsoft.Extensions.DependencyInjection.dll',
-    '_framework/_bin/Microsoft.JSInterop.dll',
+    './_framework/_bin/Microsoft.JSInterop.dll',
     './_framework/_bin/mscorlib.dll',
     './_framework/_bin/netstandard.dll',
     './_framework/_bin/System.Net.Http.dll',
     './_framework/_bin/Mono.WebAssembly.Interop.dll',
     './_framework/_bin/System.dll',
     './_framework/_bin/System.Core.dll', 
-    //Pages
-    './counter',
     //The compiled project .dll's
     './_framework/_bin/DotnetPwaSample.dll'
 ];
@@ -43,7 +39,7 @@ self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(cacheName).then(function (cache) {
             console.log('[ServiceWorker] Caching app shell');
-            return cache.addAll(filesToCache);
+            return cache.addAll(filesToCache); 
         })
     );
 });
@@ -54,7 +50,7 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request, { ignoreSearch: true }).then(response => { 
+        caches.match(event.request, { ignoreSearch: true }).then(response => {
             return response || fetch(event.request);
         })
     );
