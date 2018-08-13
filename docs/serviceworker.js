d8a0ddc0d71892e83fb377c30b0dbbe76e8b66c1
Konrad Müller <k.l.mueller@outlook.com>
2018-08-13 23:27:19 +0200

    Changed service worker cache paths

--- docs/serviceworker.js
+++ docs/serviceworker.js
@@ -1,41 +1,41 @@
 ﻿console.log("This is service worker talking");
 var cacheName = 'blazor-pwa-sample';
 var filesToCache = [
-    '/',
+    './',
     //Html and css files
-    '/index.html',
-    '/css/site.css',
-    '/css/bootstrap/bootstrap.min.css',
-    '/css/open-iconic/font/css/open-iconic-bootstrap.min.css',
-    '/open-iconic',
-    '/css/open-iconic/font/fonts/open-iconic.woff',
-    '/css/loading.css',
+    './index.html',
+    './css/site.css',
+    './css/bootstrap/bootstrap.min.css',
+    './css/open-iconic/font/css/open-iconic-bootstrap.min.css',
+    './open-iconic',
+    './css/open-iconic/font/fonts/open-iconic.woff',
+    './css/loading.css',
     //Blazor framework
     '_framework/blazor.webassembly.js',
-    '/_framework/blazor.boot.json',
+    './_framework/blazor.boot.json',
     //Our additional files
-    '/manifest.json',
-    '/serviceworker.js',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png',
+    './manifest.json',
+    './serviceworker.js',
+    './icons/icon-192x192.png',
+    './icons/icon-512x512.png',
     //The web assembly/.net dll's
-    '/_framework/wasm/mono.js',
-    '/_framework/wasm/mono.wasm', 
-    '/_framework/_bin/Microsoft.AspNetCore.Blazor.Browser.dll',
-    '/_framework/_bin/Microsoft.AspNetCore.Blazor.dll',
-    '/_framework/_bin/Microsoft.Extensions.DependencyInjection.Abstractions.dll',
-    '/_framework/_bin/Microsoft.Extensions.DependencyInjection.dll',
+    './_framework/wasm/mono.js',
+    './_framework/wasm/mono.wasm', 
+    './_framework/_bin/Microsoft.AspNetCore.Blazor.Browser.dll',
+    './_framework/_bin/Microsoft.AspNetCore.Blazor.dll',
+    './_framework/_bin/Microsoft.Extensions.DependencyInjection.Abstractions.dll',
+    './_framework/_bin/Microsoft.Extensions.DependencyInjection.dll',
     '_framework/_bin/Microsoft.JSInterop.dll',
-    '/_framework/_bin/mscorlib.dll',
-    '/_framework/_bin/netstandard.dll',
-    '/_framework/_bin/System.Net.Http.dll',
-    '/_framework/_bin/Mono.WebAssembly.Interop.dll',
-    '/_framework/_bin/System.dll',
-    '/_framework/_bin/System.Core.dll', 
+    './_framework/_bin/mscorlib.dll',
+    './_framework/_bin/netstandard.dll',
+    './_framework/_bin/System.Net.Http.dll',
+    './_framework/_bin/Mono.WebAssembly.Interop.dll',
+    './_framework/_bin/System.dll',
+    './_framework/_bin/System.Core.dll', 
     //Pages
-    '/counter',
+    './counter',
     //The compiled project .dll's
-    '/_framework/_bin/DotnetPwaSample.dll'
+    './_framework/_bin/DotnetPwaSample.dll'
 ];
 
 self.addEventListener('install', function (e) {
